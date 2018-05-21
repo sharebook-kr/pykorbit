abac8e362a321c26fcc8337e55b6881b94dbfc94
brayden.jo <brayden.jo@outlook.com>
2018-05-21 18:32:20 +0900

    0.1.0 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name            = 'pykorbit',
-    version         = '0.0.9',
+    version         = '0.1.0',
     description     = 'python wrapper for Korbit API',
     url             = 'https://github.com/sharebook/pykorbit',
     author          = 'brayden.jo',
