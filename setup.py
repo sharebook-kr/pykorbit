00954a7e36ceb7a699d3cf191a3b33b3f3c1aa84
brayden-jo <brayden.jo@outlook.com>
2018-06-20 23:15:38 +0900

    add get_tickers

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name            = 'pykorbit',
-    version         = '0.1.4',
+    version         = '0.1.5',
     description     = 'python wrapper for Korbit API',
     url             = 'https://github.com/sharebook-kr/pykorbit',
-    author          = 'brayden.jo',
-    author_email    = 'brayden.jo@outlook.com',
+    author          = 'brayden.jo, Lukas Yoo',
+    author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com',
     license         = 'MIT',
     packages        = ['pykorbit'],
     zip_safe        = False
