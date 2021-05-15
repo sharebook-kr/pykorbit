dfa5aef27f50bce38e6e35e5b2d799ca98d1c9d8
jonghun.yoo <jonghun.yoo@outlook.com>
2021-05-15 13:12:00 +0900

    dependency가 있는 모듈을 install시 다운 되도록 설정 (#3)

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url             = 'https://github.com/sharebook-kr/pykorbit',
     author          = 'brayden.jo, jonghun.yoo',
     author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com',
+    install_requires= ['requests', 'pandas', 'datetime', 'numpy', 'xlrd', 'PyJWT'],
     license         = 'MIT',
     packages        = ['pykorbit'],
     zip_safe        = False
